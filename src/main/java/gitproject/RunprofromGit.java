a3998f22548035373a0683c313dadb60d659caef
Shivanand.chachadi <shivu.2007@gmail.com>
2023-02-14 10:50:38 +0530

    added maven porject instead of html

---
+++ src/main/java/gitproject/RunprofromGit.java
@@ -0,0 +1,12 @@
+package gitproject;
+
+public class RunprofromGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Running project directly from github");
+	}
+
+}
